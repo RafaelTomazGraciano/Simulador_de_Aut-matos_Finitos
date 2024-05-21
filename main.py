bdc57d5c496f5413f035376135dc311c1acfc909
Rafael Tomaz Graciano <rafaeltomaz2005@gmail.com>
2024-05-21 20:28:53 -0300

    Projeto Finalizado

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ import csv
 import time
 import sys
 
+if len(sys.argv) != 4:
+    print("Use: python main.py arquivo_do_automato.aut arquivo_de_testes.in arquivo_de_saida.out")
+    sys.exit(1)
 
 def delta(q, a, transicoes):
+    resultados = []
     for transicao in  transicoes:
         if transicao['from'] == q and transicao['read'] == a:
-            return transicao['to']
-    return -1;
+            resultados.append(transicao['to'])
+    return resultados
     
 
 #inicia contar o tempo
 inicio_tempo = time.perf_counter()
 
 # Lendo JSON
-arquivo_json = open("arquivo_do_automato.aut")#sys.argv[1]
+arquivo_json = open(sys.argv[1])
  
 # retorna o JSON 
 dadosJson = json.load(arquivo_json)
@@ -24,27 +28,34 @@ estadoInicial = dadosJson['initial']
 estadoFinal = dadosJson['final']
 transicoes = dadosJson['transitions']
 
- 
 
 
-with open('arquivo_de_saida.out', 'w') as arquivo_out: #criando arquivo out
-    with open('arquivo_de_testes.in', 'r') as arquivo_in: #lendo arquivo in
+with open(sys.argv[3], 'w', newline='') as arquivo_out: #criando arquivo out
+    with open(sys.argv[2], 'r') as arquivo_in: #lendo arquivo in
         leitor = csv.reader(arquivo_in, delimiter = ';')
         for linha in leitor:
-            estadoAtual = estadoInicial
+            estadosAtuais = [estadoInicial]
             palavra = linha[0]
             caracteres = list(palavra)
             print(caracteres)
+            estados = []
             for caractere in caracteres:
-                estadoAtual = delta(estadoAtual, caractere, transicoes)
-                print('q' + str(estadoAtual) + ' - ' + caractere)
-                if estadoAtual == -1:
+                novos_estados = []
+                for estado_atual in estadosAtuais:
+                    novos_estados.extend(delta(estado_atual, caractere, transicoes))
+                estadosAtuais = novos_estados
+                estados.append(estadosAtuais)
+                print(estados)
+                print('q' + str(estadosAtuais) + ' - ' + caractere)
+                if estadosAtuais == -1:
                     break
-            print(estadoAtual)    
-            if estadoAtual == estadoFinal:
-                resultado_obtido = 1
-            else:
-                resultado_obtido = 0
+            print(estadosAtuais)
+            for estado in estados:    
+                if estadoFinal in estado:
+                    resultado_obtido = 1
+                    break
+                else:
+                    resultado_obtido = 0
             #Escrevendo arquivo out
             escritor = csv.writer(arquivo_out, delimiter = ';')
             fim_tempo = time.perf_counter()
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-class Automato:
-#     def _init_(self, Q, Sigma, delta, inicio, final):
-#         self.Q = Q #estados
-#         self.Sigma = Sigma # simbolos
-#         self.delta = delta # transição
-#         self.inicio = inicio # estados iniciais
-#         self.final = final # estados finais
