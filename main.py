82c63ffe3d3a962ef18fe8a0cf67e7f874770ff6
Rafael Tomaz Graciano <rafaeltomaz2005@gmail.com>
2024-05-23 09:10:09 -0300

    Projeto Finalizado

--- main.py
+++ main.py
@@ -9,27 +9,25 @@ if len(sys.argv) != 4: #Trantando os arquivos de entrada
     sys.exit(1)
 
 
-def delta(q, a, transicoes):#Função delta que executa as transicoes do automato
+def delta(q, a, transicoes): #Função delta que executa as transicoes do automato
     resultados = []
     for transicao in  transicoes:
-        if transicao['from'] == q and transicao['read'] == a or transicao['read'] == None:
-            resultados.append(transicao['to'])
+        if int(transicao['from']) == q and transicao['read'] == a or transicao['read'] == None:
+            resultados.append(int(transicao['to']))
     return resultados
 
 
-def deterministico(dadosJson):#Verifica se o automato e deterministico ou nao deterministico
+def e_deterministico(dadosJson): #Verifica se o automato e deterministico ou nao deterministico
     estados = set()
     transicoes = set() 
     for transicao in dadosJson['transitions']:
-        estados.add(transicao['from'])
-        transicao_atual = (transicao['from'], transicao['read'])
+        estados.add(int(transicao['from']))
+        transicao_atual = (int(transicao['from']), transicao['read'])
         if transicao_atual in transicoes:
             return False 
         transicoes.add(transicao_atual)
     return True
 
-def remover_duplicatas(lista):
-    return list(dict.fromkeys(lista))
 
 #inicia contar o tempo
 inicio_tempo = time.perf_counter() #Inicia a contar o tempo
@@ -40,9 +38,11 @@ arquivo_json = open(sys.argv[1])
 # retorna o JSON 
 dadosJson = json.load(arquivo_json) 
 
+deterministico = e_deterministico(dadosJson)
+
 #definindo os estados
 estadoInicial = dadosJson['initial']
-estadoFinal = [(dadosJson['final'])]
+estadoFinal = [(final_state) for final_state in dadosJson['final']]
 transicoes = dadosJson['transitions']
 
 with open(sys.argv[3], 'w', newline='') as arquivo_out: #criando arquivo out
@@ -59,10 +59,11 @@ with open(sys.argv[3], 'w', newline='') as arquivo_out: #criando arquivo out
                 for estado_atual in estadosAtuais:
                     novos_estados.extend(delta(estado_atual, caractere, transicoes))
                 estadosAtuais = novos_estados #os estadosAtuais recebe os novos_estados
-                estados.append(remover_duplicatas(estadosAtuais))
+                estados.append(estadosAtuais)
+                #print(estados)
                 if estadosAtuais == -1:
                     break
-            if deterministico(dadosJson) == True: 
+            if deterministico == True: 
                 for estado in estados: #Verificando se o automato chegou no estado final para automato deterministico
                     for final in estadoFinal:
                         if final in estado:
@@ -85,6 +86,5 @@ with open(sys.argv[3], 'w', newline='') as arquivo_out: #criando arquivo out
             escritor = csv.writer(arquivo_out, delimiter = ';')
             fim_tempo = time.perf_counter() 
             tempo = fim_tempo - inicio_tempo #calcula o tempo
-            tempo = f"{tempo:.4f}"
             escritor.writerow([linha[0], linha[1], resultado_obtido, tempo])
     
